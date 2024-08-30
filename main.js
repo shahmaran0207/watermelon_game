0dd6c50b814413758a69f6d9f6a615bc12f09534
shahmaran0207 <shahmaran0207@naver.com>
2024-08-30 09:39:48 +0900

    기본 틀 완성

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-import { Engine, Render, Runner } from "matter-js";
+import { Bodies, Engine, Render, Runner, World } from "matter-js";
 
 const engine=Engine.create();
 const render=Render.create({
     engine,
     element: document.body,
     options:{
+        wireframes: false,
         background: "#F7F4C8",
         width: 620,
         height: 850,
     }
 });
+
+const world=engine.world;
+
+const leftWall=Bodies.rectangle(15, 395, 30, 790, {
+    isStatic: true,
+    render: {fillStyle:"#E6B143"}
+});
+
+const rightWall=Bodies.rectangle(605, 395, 30, 790, {
+    isStatic: true,
+    render: {fillStyle:"#E6B143"}
+});
+
+const ground=Bodies.rectangle(310, 820, 620, 60, {
+    isStatic: true,
+    render: {fillStyle:"#E6B143"}
+});
+
+World.add(world, [leftWall, rightWall, ground]);
 
 Render.run(render);
 Runner.run(engine);
