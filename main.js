b6c2a7a7a343a194fd08ccba11667b9a9efec84d
shahmaran0207 <shahmaran0207@naver.com>
2024-08-30 09:27:55 +0900

    기본 엔진 설정

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import { Engine, Render, Runner } from "matter-js";
+
+const engine=Engine.create();
+const render=Render.create({
+    engine,
+    element: document.body,
+    options:{
+        background: "#F7F4C8",
+        width: 620,
+        height: 850,
+    }
+});
+
+Render.run(render);
+Runner.run(engine);
