1830a516b7340c54eea7bca1d021fd61701da69e
shahmaran0207 <shahmaran0207@naver.com>
2024-08-30 10:46:42 +0900

    다음 과일 생성

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import { Body, Bodies, Engine, Render, Runner, World } from "matter-js";
-import {FRUITS_HLW} from "./fruits.js";
+import { Body, Bodies, Engine, Render, Runner, World, Events } from "matter-js";
+import { FRUITS_HLW } from "./fruits.js";
 
-const engine=Engine.create();
-const render=Render.create({
+const engine = Engine.create();
+const render = Render.create({
     engine,
     element: document.body,
-    options:{
+    options: {
         wireframes: false,
         background: "#F7F4C8",
         width: 620,
@@ -13,80 +13,114 @@ const render=Render.create({
     }
 });
 
-const world=engine.world;
+const world = engine.world;
 
-const leftWall=Bodies.rectangle(15, 395, 30, 790, {
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
     isStatic: true,
-    render: {fillStyle:"#E6B143"}
+    render: { fillStyle: "#E6B143" }
 });
 
-const rightWall=Bodies.rectangle(605, 395, 30, 790, {
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
     isStatic: true,
-    render: {fillStyle:"#E6B143"}
+    render: { fillStyle: "#E6B143" }
 });
 
-const ground=Bodies.rectangle(310, 820, 620, 60, {
+const ground = Bodies.rectangle(310, 820, 620, 60, {
     isStatic: true,
-    render: {fillStyle:"#E6B143"}
+    render: { fillStyle: "#E6B143" }
 });
 
-const topLine=Bodies.rectangle(310, 150, 620, 2, {
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
     isStatic: true,
     isSensor: true,
-    render: {fillStyle:"#E6B143"}
+    render: { fillStyle: "#E6B143" }
 });
-
 
 World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
 Runner.run(engine);
 
-let currentbBody=null;
-let currentFruit=null;
+let currentBody = null;
+let currentFruit = null;
+let disableAction = false;
 
-function addFruit(){
-    const index=Math.floor(Math.random()*5);
-    const fruit=FRUITS_HLW[index];
+function addFruit() {
+    const index = Math.floor(Math.random() * 5);
+    const fruit = FRUITS_HLW[index];
 
-    const body=Bodies.circle(300, 50, fruit.radius, {
+    const body = Bodies.circle(300, 50, fruit.radius, {
         index: index,
-        isSleeping: true,       //사용자가 원하는 위치에 조정하기 전까지 대기시키는 것
+        isSleeping: true,
         render: {
-            sprite: {texture: `${fruit.name}.png`}
+            sprite: { texture: `${fruit.name}.png` }        //이미지 텍스처를 적용하기 위해 사용
         },
-        restitution: 0.3, //탄성  0~1 사이
+        restitution: 0.35, // 탄성 0~1 사이
     });
 
-    currentbBody=body;
-    currentFruit=fruit;
+    currentBody = body;
+    currentFruit = fruit;
 
     World.add(world, body);
 }
 
-window.onkeydown=(event)=>{
+window.onkeydown = (event) => {
+    if (disableAction) return;
+
+    const { x, y } = currentBody.position;
+    const radius = currentFruit.radius;
+
     switch (event.code) {
         case "ArrowLeft":
-            Body.setPosition(currentbBody,
-                {
-                    x: currentbBody.position.x-10,
-                    y: currentbBody.position.y,
-                });
+            if (x - radius > 30) {
+                Body.setPosition(currentBody, { x: x - 10, y });
+            }
             break;
 
         case "ArrowRight":
-            Body.setPosition(currentbBody,
-                {
-                    x: currentbBody.position.x+10,
-                    y: currentbBody.position.y,
-                });
+            if (x + radius < 590) {
+                Body.setPosition(currentBody, { x: x + 10, y });
+            }
             break;
 
         case "ArrowDown":
-            currentbBody.isSleeping=false;
-            break;
+            currentBody.isSleeping = false;
+            disableAction = true;
 
+            setTimeout(() => {
+                addFruit();
+                disableAction = false;
+            }, 1500);
+            break;
     }
-}
+};
+
+Events.on(engine, "collisionStart", (event) => {
+    event.pairs.forEach((collision) => {
+        if (collision.bodyA.index === collision.bodyB.index) {
+            const index=collision.bodyA.index;
+
+            if (index===FRUITS_HLW.length-1) return;
+
+            World.remove(world, [collision.bodyA, collision.bodyB]);
+
+            const newFruit=FRUITS_HLW[index+1];
+
+            const newBody=Bodies.circle(
+                collision.collision.supports[0].x,
+                collision.collision.supports[0].y,
+                newFruit.radius,
+                {
+                    render: {
+                        sprite: { texture: `${newFruit.name}.png` }        //이미지 텍스처를 적용하기 위해 사용
+                    },
+                    index: index+1,
+                }
+            );
+
+            World.add(world, newBody);
+        }
+    });
+});
 
 addFruit();
